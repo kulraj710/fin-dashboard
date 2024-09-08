967a03c0c72f83f59319495ed19499a7436c7d03
kulraj710 <kulraj710@gmail.com>
2024-09-08 16:48:51 +0530

    [feat] : add function to get RSI data

---
+++ backend/services/rsi.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import pandas_datareader as web
+import yfinance as yf
+import datetime as dt
+
+def get_rsi_data_and_stock_price(ticker='GOOG', start=dt.datetime(2024,1,1), end=dt.datetime.now()):
+    try:
+        data = yf.download(ticker,start=start, end=end)
+        delta = data['Adj Close'].diff(1)
+        delta.dropna(inplace=True)
+        positive = delta.copy()
+        negative = delta.copy()
+
+        # scrap negavite values
+        positive[ positive < 0] = 0
+
+        # scrap positive values
+        negative[ negative > 0] = 0
+
+        days = 14
+
+        avgerate_gain = positive.rolling(window=days).mean()
+        avgerate_loss = abs(negative.rolling(window=days).mean())
+
+        # calc relative strength 
+        relative_strength = avgerate_gain / avgerate_loss
+        RSI = 100.0 - (100.0 / (1.0 + relative_strength))
+
+        combined = pd.DataFrame()
+        combined['AdjClose'] = data['Adj Close']
+        combined['rsi'] = round(RSI, 3)
+        combined['date'] = data.index.strftime('%d-%m')
+
+    
+        clean_combined = combined.dropna(axis=0, how='any')
+    
+        rsi_data_json = clean_combined.to_dict(orient='records')
+        
+        return rsi_data_json
+    except Exception as e:
+        print(e)
+        return "Something went wrong"
