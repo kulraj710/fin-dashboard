037e0d5a5042cca8f73807837a1d5cf1b4820046
kulraj710 <kulraj710@gmail.com>
2024-09-13 11:52:49 +0530

    [api] : update CORS

--- backend/app.py
+++ backend/app.py
@@ -5,20 +5,21 @@ from api.backtest import BacktestStatAPI, PlotFileAPI
 from api.blackscholes import BlackScholesPricing
 from flask_cors import CORS
 from config.config import get_config
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 
 # Load environment variables from .env
-load_dotenv()
+# load_dotenv()
 
 # Initialize Flask app
 app = Flask(__name__)
 
 # Load config based on environment
-app.config.from_object(get_config())
+# app.config.from_object(get_config())
 
 
-CORS(app, resources={r"/api/*": {"origins": app.config['CORS_ALLOWED_ORIGINS']}})
+# CORS(app, resources={r"/api/*": {"origins": app.config['CORS_ALLOWED_ORIGINS']}})
+CORS(app, resources={r"/api/*": {"origins": ['https://finance-tools-alpha.vercel.app', 'http://localhost:5000']}})
 
 api = Api(app)
 
