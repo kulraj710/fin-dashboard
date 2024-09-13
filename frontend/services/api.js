7cbf675dc4c8cd4ed8e7750694b997983bb043af
kulraj710 <kulraj710@gmail.com>
2024-09-13 12:57:43 +0530

    [build] update env vars

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-
 const axiosInstance = axios.create({
-  baseURL: process.env.backendBaseUrlProduction,
+  baseURL: NEXT_PUBLIC_backendBaseUrlProduction,
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
 });
