016b7b100da1744f5991497fe8ca9e79637161dc
kulraj710 <kulraj710@gmail.com>
2024-09-25 00:09:12 +0530

    [refactor] remove comments, redirect to frontend

--- backend/app.py
+++ backend/app.py
@@ -1,35 +1,18 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask_restful import Api
 from api.rsi import RsiAPI
 from api.backtest import BacktestStatAPI, PlotFileAPI
 from api.blackscholes import BlackScholesPricing
 from api.montecarlo import MonteCarloPricing
-from flask_cors import CORS
-from config.config import get_config
-# from  utils.CustomException import NoTickerFoundException, handle_507
-# import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env
-load_dotenv()
 
 # Initialize Flask app
 app = Flask(__name__)
 
-# Load config based on environment
-app.config.from_object(get_config())
-
-# app.register_error_handler(NoTickerFoundException, handle_507)
-
-
-allowed_origins = app.config['CORS_ALLOWED_ORIGINS']
-
-print(allowed_origins)
-
-CORS(app, resources={r"/api/*": {"origins": allowed_origins}})
-# CORS(app, resources={r"/api/*": {"origins": ['https://finance-tools-alpha.vercel.app', 'http://localhost:3000']}})
-
 api = Api(app)
+
+@app.route('/')
+def index():
+    return redirect("https://finance-tools-alpha.vercel.app")
 
 # Register API Resources
 api.add_resource(RsiAPI, '/api/rsi')
